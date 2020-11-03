07f5da4fbd4caf0abfebbcdd1bbd86d8b6c0f93c
kuzinkim <kuzinhb2019@gmail.com>
2020-11-03 08:13:07 +0300

    адаптив планшетка

--- app/js/common.js
+++ app/js/common.js
@@ -5,116 +5,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // инициализация плагина плавной прокрутки по якорю
     initSmoothScrolling();
     
-    tab();
-
-    // функционал вызова и закрытия модального окна
-    var popupLink = document.querySelectorAll('.js-popup-show'),
-        popupClose = document.querySelectorAll('.js-close');
-    body = document.querySelector('body');
-    lockPadding = document.querySelectorAll('.lock-padding');
-    unlock = true
-    timeOut = 400
-
-    if (popupLink.length > 0) {
-
-        for (var i = 0; i < popupLink.length; i++) {
-            var popupLinkElement = popupLink[i]
-
-            popupLinkElement.addEventListener("click", function (e) {
-                var linkId = this.getAttribute('data-content');
-                var popupElement = document.querySelector('' + linkId);
-                popupOpen(popupElement)
-                e.preventDefault()
-            })
-        }
-    }
-
-    if (popupClose.length > 0) {
-
-        for (var i = 0; i < popupClose.length; i++) {
-            var popupCloseElement = popupClose[i]
-
-            popupCloseElement.addEventListener("click", function (e) {
-                popupOnClose(this.closest('.popup'), true)
-                e.preventDefault()
-            })
-        }
-    }
-
-    function popupOpen(curentPopup) {
-
-        if (curentPopup && unlock) {
-
-            var popupActive = document.querySelector('.popup.open');
-
-            if (popupActive) {
-                popupOnClose(popupActive, false);
-            } else {
-                bodyLock();
-            }
-
-            curentPopup.classList.add('open')
-            curentPopup.addEventListener('click', function (e) {
-                if (!e.target.closest('.popup__content')) {
-                    popupOnClose(e.target.closest('.popup'), true)
-                }
-            })
-        }
-    }
-
-    function bodyLock() {
-        var lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + "px";
-        if (lockPadding.length > 0) {
-
-            for (var i = 0; i < lockPadding.length; i++) {
-                var lockPaddingEl = lockPadding[i]
-                lockPaddingEl.style.paddingRight = lockPaddingValue
-            }
-        }
-        body.style.paddingRight = lockPaddingValue
-        body.classList.add('is-hidden')
-        buttonUp.style.opacity = 0
-
-        unlock = false
-
-        setTimeout(function () {
-            unlock = true
-        }, timeOut)
-
-    }
-
-    function bodyUnLock() {
-
-        setTimeout(function () {
-            for (var i = 0; i < lockPadding.length; i++) {
-                var lockPaddingEl = lockPadding[i]
-                lockPaddingEl.style.paddingRight = '0px'
-            }
-
-            body.style.paddingRight = '0px'
-            body.classList.remove('is-hidden')
-            buttonUp.style.opacity = 1
-
-        }, timeOut)
-
-        unlock = false
-
-        setTimeout(function () {
-            unlock = true
-        }, timeOut)
-    }
-
-    function popupOnClose(popupActive, doUnlock) {
-
-        if (unlock) {
-            popupActive.classList.remove('open');
-
-            if (doUnlock) {
-                bodyUnLock();
-            }
-        }
-    }
-
     var bindExpandService = function (block) {
         block.classList.add('is-hidden');
         var button = block.querySelector('.js-more-btn');
@@ -176,6 +66,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
     };
 
+    tab();
+
     var buttonUp = document.querySelector('.button-up')
     
     window.addEventListener('scroll', function(e){
@@ -198,6 +90,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     })
 
+    @@include('../components/popup/popup.js')
     @@include('../components/popup/form.js')
 
 });
